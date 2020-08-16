9bc5e356d43c63c726826bb2b79673f617536c9a
ibrahim AKIN <ibrahim-akin@msn.com>
2020-08-16 11:46:30 +0300

    Add movies page

---
+++ movies/precache-manifest.84c0e404b7296ac32c42c0494811ad45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa26b900b2588ca301c4352e29884e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d9adcd0a3c556b0e75",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "fa495a5ea3978a171562",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9d9adcd0a3c556b0e75",
+    "url": "/static/js/2.0b22c489.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0b22c489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa495a5ea3978a171562",
+    "url": "/static/js/main.3e887e12.chunk.js"
+  },
+  {
+    "revision": "2e71319991222b421a36",
+    "url": "/static/js/runtime-main.22e28c68.js"
+  }
+]);
