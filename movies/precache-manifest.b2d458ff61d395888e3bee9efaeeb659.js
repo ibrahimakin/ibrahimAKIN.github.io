154a0a6c269241d99957595794d818fd350bdd4e
ibrahim AKIN <ibrahim-akin@msn.com>
2020-08-17 22:49:29 +0300

    update movies page

--- movies/precache-manifest.b2d458ff61d395888e3bee9efaeeb659.js
+++ movies/precache-manifest.b2d458ff61d395888e3bee9efaeeb659.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa26b900b2588ca301c4352e29884e61",
+    "revision": "6aa5a937ab7abc40537760ee0d70670f",
     "url": "/index.html"
   },
   {
-    "revision": "e9d9adcd0a3c556b0e75",
+    "revision": "32af9ad77c573c461acc",
     "url": "/static/css/2.04ff9426.chunk.css"
   },
   {
-    "revision": "fa495a5ea3978a171562",
+    "revision": "2381dd1f534a52faba9c",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "e9d9adcd0a3c556b0e75",
-    "url": "/static/js/2.0b22c489.chunk.js"
+    "revision": "32af9ad77c573c461acc",
+    "url": "/static/js/2.7cf6f19f.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.0b22c489.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7cf6f19f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fa495a5ea3978a171562",
-    "url": "/static/js/main.3e887e12.chunk.js"
+    "revision": "2381dd1f534a52faba9c",
+    "url": "/static/js/main.ebf9b085.chunk.js"
   },
   {
     "revision": "2e71319991222b421a36",
--- movies/service-worker.js
+++ movies/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70ce893036eabd872dbf16fd8eb1cf5e.js"
+  "/precache-manifest.b2d458ff61d395888e3bee9efaeeb659.js"
 );
 
 self.addEventListener('message', (event) => {
