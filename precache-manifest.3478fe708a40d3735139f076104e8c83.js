8453edaf8fda08501ac57a7646c90763f9cea4fd
ibrahim AKIN <ibrahim-akin@msn.com>
2020-09-23 01:45:15 +0300

    Add multi language option

---
+++ precache-manifest.3478fe708a40d3735139f076104e8c83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f068188f291f9e1e9ef6efc6334b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22fa07429744b63b8c4e",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "c53cdfedf31a2064beae",
+    "url": "/static/css/main.42305418.chunk.css"
+  },
+  {
+    "revision": "22fa07429744b63b8c4e",
+    "url": "/static/js/2.39e08061.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.39e08061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53cdfedf31a2064beae",
+    "url": "/static/js/main.2ae1393c.chunk.js"
+  },
+  {
+    "revision": "1dba3009e3472122a76e",
+    "url": "/static/js/runtime-main.5b555d20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
