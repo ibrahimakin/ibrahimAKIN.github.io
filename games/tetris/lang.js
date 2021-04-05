2c137e3a17f6b1af9a6caa9a678a0294f8daf153
ibrahim AKIN <ibrahim-akin@msn.com>
2021-04-05 23:49:49 +0300

    Update tetris game

--- games/tetris/lang.js
+++ games/tetris/lang.js
@@ -1,19 +1,24 @@
-export const tr = {
-    score: 'Puan',
-    rows: 'Rows',
-    level: 'Seviye',
-    start_game: 'Oyuna Basla',
-    game_over: 'Oyun Bitti',
-    best_score: 'Best Score',
-    max_rows: "Max Rows"
-}
-
-export const en = {
-    score: 'Score',
-    rows: 'Rows',
-    level: 'Level',
-    start_game: 'Start Game',
-    game_over: 'Game Over',
-    best_score: 'Best Score',
-    max_rows: "Max Rows"
+const langObj = {
+    tr : {
+        score: 'Skor',
+        rows: 'Rows',
+        level: 'Seviye',
+        start_game: 'Oyuna Basla',
+        game_over: 'Oyun Bitti',
+        high_score: 'En Yüksek Skor',
+        max_rows: 'Max Rows',
+        pause_game: 'Durdur',
+        resume_game: 'Devam Et'
+    },
+    en: {
+        score: 'Score',
+        rows: 'Rows',
+        level: 'Level',
+        start_game: 'Start Game',
+        game_over: 'Game Over',
+        high_score: 'High Score',
+        max_rows: 'Max Rows',
+        pause_game: 'Pause Game',
+        resume_game: 'Resume Game'
+    }
 }
