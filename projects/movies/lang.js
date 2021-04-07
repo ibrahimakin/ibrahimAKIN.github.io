05f87e41ecac6513d0589fc48daa8906c065762d
ibrahim AKIN <ibrahim-akin@msn.com>
2021-04-07 19:38:17 +0300

    Add lang option to movies

---
+++ projects/movies/lang.js
@@ -0,0 +1,18 @@
+const langObj = {
+    'tr': {
+        'movies': 'FİLMLER',
+        'search': 'Ara',
+        'year': 'Yıl',
+        'details': 'Detaylar',
+        'director': 'Yönetmen',
+        'cast': 'Oyuncular'
+    },
+    'en': {
+        'movies': 'MOVIES',
+        'search': 'Search',
+        'year': 'Year',
+        'details': 'Details',
+        'director': 'Director',
+        'cast': 'Cast'
+    }
+};
