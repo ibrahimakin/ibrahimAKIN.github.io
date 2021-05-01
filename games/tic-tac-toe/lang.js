276b8f48b5951e1bd1b9a43bbf8381bd41631f03
ibrahim AKIN <ibrahim-akin@msn.com>
2021-05-01 17:44:25 +0300

    Update tic tac toe game

---
+++ games/tic-tac-toe/lang.js
@@ -0,0 +1,16 @@
+const langObj = {
+    tr: {
+        next_player: 'Sıradaki Oyuncu: ',
+        go_to_start: 'Oyun başlangıcına git',
+        go_to_move: 'Hamleye git #',
+        winner: 'Kazanan: ',
+        draw: 'Berabere'
+    },
+    en: {
+        next_player: 'Next Player: ',
+        go_to_start: 'Go to game start',
+        go_to_move: 'Go to move #',
+        winner: 'Winner: ',
+        draw: 'Draw'
+    }
+}
