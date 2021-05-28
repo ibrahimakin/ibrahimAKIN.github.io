43a5079886e47a34a76175f5e192595a61a73430
ibrahim AKIN <ibrahim-akin@msn.com>
2021-05-28 10:30:52 +0300

    Update LCD Character Generator

--- projects/lcd-character-generator/lcd-character-generator.js
+++ projects/lcd-character-generator/lcd-character-generator.js
@@ -28,24 +28,29 @@ function btnClear() {
 		}
 	}
 }
-
+let displayResult = 0;
+const displayRes = document.getElementById('dr');
 function DisplayClick(btn) {
+	let value = parseInt(btn.value);
 	if (btn.style.backgroundColor != 'black') {
 		btn.style.backgroundColor = 'black';
 		btn.style.color = 'white';
 		document.getElementById(btn.name).innerHTML = 1;
+		displayResult += value;
 	}
 	else {
 		btn.style.backgroundColor = '';
 		btn.style.color = '';
 		document.getElementById(btn.name).innerHTML = 0;
+		displayResult -= value;
 	}
+	displayRes.innerHTML = displayResult;
 }
 let dizi = ['na', 'b', 'c', 'd', 'e', 'f', 'g', 'dp'];
 function displayClear() {
-    console.log('displayClear', dizi)
-	let i;
-	for (i = 0; i < 8; i++) {
+	displayResult = 0;
+	displayRes.innerHTML = 0;
+	for (let i = 0; i < 8; i++) {
 		document.getElementById("i" + dizi[i]).style.backgroundColor = 'white';
 		document.getElementById("i" + dizi[i]).style.color = 'black';
 		document.getElementById(dizi[i]).innerHTML = 0;
