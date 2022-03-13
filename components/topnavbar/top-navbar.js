3a27f84b134b2b8c36ea1f165f48a5230b60d359
ibrahim AKIN <ibrahim-akin@msn.com>
2022-03-13 13:14:39 +0300

    update project name

--- components/topnavbar/top-navbar.js
+++ components/topnavbar/top-navbar.js
@@ -15,7 +15,7 @@ class Navbar extends HTMLElement {
               <div class="dropdown-top tn-same-width">
                 <button title="Projects" class="white ripple-button project icon" name="projects"></button>
                 <div id="projectstdc" class="dropdown-content">
-                  <a title="English Learner" href="/projects/english-learner"><button class="white smile icon"></button></a>
+                  <a title="Language App" href="/projects/english-learner"><button class="white smile icon"></button></a>
                   <a title="Code Playground" href="/projects/code-playground"><button class="white code icon"></button></a>
                   <a title="Face Detector" href="/projects/face-detector"><button class="white face icon"></button></a>
                   <a title="LCD Character Generator" href="/projects/lcd-character-generator"><button class="white iicon icon"></button></a>
--- projects/lang.js
+++ projects/lang.js
@@ -4,13 +4,13 @@ const langObj = {
         movies: 'Filmler',
         face_detector: 'Yüz Tanıyıcı',
         code_playground: 'Code Playground',
-        english_learner: 'English Learner'
+        language_app: 'Language App'
     },
     en: {
         lcd: 'LCD Character Generator',
         movies: 'Movies',
         face_detector: 'Face Detector',
         code_playground: 'Code Playground',
-        english_learner: 'English Learner'
+        language_app: 'Language App'
     }
 };
