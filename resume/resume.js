3aa9b7e0e41b05b5343f05114c5255381ae13541
ibrahim AKIN <ibrahim-akin@msn.com>
2022-04-23 17:15:39 +0300

    delete unnecessary files

--- resume/resume.js
+++ resume/resume.js
@@ -1,43 +1,36 @@
-function readMoreFunction(event) {
-    let dots = document.getElementById("more-courses-dots");
-    let moreText = document.getElementById("more-courses");
-    let btnMore = event.target;
-    let btnLess = document.getElementById("read-less-courses");
-  
-    if (dots.style.display !== "none") {
-        dots.style.display = "none";
-        btnMore.style.display = 'none';
+function readMore(e) {
+    let dots = document.getElementById('more-courses-dots');
+    let moreText = document.getElementById('more-courses');
+    let btnLess = document.getElementById('read-less-courses');
+
+    if (dots.style.display !== 'none') {
+        dots.style.display = 'none';
+        e.target.style.display = 'none';
         btnLess.style.display = 'inline';
-        moreText.style.display = "inline";
+        moreText.style.display = 'inline';
     }
 }
 
-function readLessFunction(event) {
-    let dots = document.getElementById("more-courses-dots");
-    let moreText = document.getElementById("more-courses");
-    let btnLess = event.target;
-    let btnMore = document.getElementById("read-more-courses");
+function readLess(e) {
+    let dots = document.getElementById('more-courses-dots');
+    let moreText = document.getElementById('more-courses');
+    let btnMore = document.getElementById('read-more-courses');
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnLess.style.display = 'none';
+    if (dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        e.target.style.display = 'none';
         btnMore.style.display = 'inline';
-        moreText.style.display = "none";
-    } 
+        moreText.style.display = 'none';
+    }
 }
 
-function responsiveResume(x, onStart){
+function responsiveResume(x, onStart) {
     let e = document.getElementById('resume-rightside');
-    // let e = document.getElementById('education');
     if (x.matches) {
-        if (onStart) {
-            e.scrollIntoView({behavior: 'smooth'});
-        }
-        else {
-            e.scrollIntoView(); // {behavior: 'smooth'}
-        }
+        if (onStart) e.scrollIntoView({ behavior: 'smooth' });
+        else e.scrollIntoView();
     }
 }
 let x = window.matchMedia('(max-width: 600px)');
-responsiveResume(x);//, true) // Call listener function at run time
+responsiveResume(x);                            // Call listener function at run time
 x.addEventListener('change', responsiveResume); // Attach listener function on state changes
