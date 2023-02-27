0c347410257ecb7016856f63f5685a9e789af57d
ibrahimakin <ibrahim-akin@msn.com>
2023-02-27 09:27:03 +0300

    refactoring

--- projects/code-playground/code-playground.js
+++ projects/code-playground/code-playground.js
@@ -1,30 +1,3 @@
-/**
-  * The code is a proof of concept and far from
-  * perfect. You should never use regex but create or use a parser.
-  * Meanwhile, play with it and improve it!
-  */
-const lang = {
-  js: {
-    equa: /(\b=\b)/g,
-    quot: /(`|'|"|&#39;|&#34;)/g,
-    comm: /((\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+\/)|(\/\/.*))/g,
-    logi: /(%=|%|\-|\+|\*|&amp;{1,2}|\|{1,2}|&lt;=|&gt;=|&lt;|&gt;|!={1,2}|={2,3})/g,
-    numb: /(\d+(\.\d+)?(e\d+)?)/g,
-    func: /(?<=^|\s*)(async|console|alert|Math|Object|Array|String|class(?!\s*\=)|function|(?<=\.)\D\w*)(?=\b)/g,
-    decl: /(?<=^|\s*)(var|let|const)/g,                                           // Declarations
-    pare: /(\(|\))/g,
-    squa: /(\[|\])/g,
-    curl: /(\{|\})/g
-  },
-  // Props order matters! Here I rely on "tags:"
-  // being already applied in the previous iteration
-  html: {
-    tags: /(?<=&lt;(?:\/)?)(\w+)(?=\s|\&gt;)/g,
-    angl: /(&lt;\/?|&gt;)/g,
-    attr: /((?<=<i class=html_tags>\w+<\/i>)[^<]+)/g
-  }
-};
-
 let iframeOMid = 'OM300x250' + Math.round(Math.random() * (10000 - 1)) + 1;       // create multi unique iframe
 let iframeOM = document.createElement('IFRAME');
 iframeOM.id = iframeOMid;
@@ -38,60 +11,65 @@ iframeOM.margin = '0';
 iframeOM.frameborder = '0';
 
 const codeResult = document.getElementById('code-result').appendChild(iframeOM);  // this can not apply to curent div
-const editors = document.querySelectorAll('.highlite_editable');
-const runButton = document.getElementById('run-button');
 const instantRun = document.getElementById('instant-run');
-
-const replaceChar = innerHtml => {
-  return innerHtml.replace(/<div>/g, '').replace(/<\/div>/g, '').replace(/<br>/g, '').replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-};
+const runButton = document.getElementById('run-button');
+const editors = document.querySelectorAll('.editor');
 
 let insantCompile = false;
-let htmlResult = replaceChar(editors[0].innerHTML);
-let scriptResult = replaceChar(editors[1].innerHTML);
+let htmlResult = editors[0].value;
+let cssResult = editors[1].value;
+let scriptResult = editors[2].value;
 
 const insertResults = () => {
-  let contentDOM = '<html><head><style>*,*::before,*::after{box-sizing:border-box;}html{overflow:auto;}body{margin:0;padding:0;height:100%;overflow:auto;}</style></head><body>' + htmlResult + '<script type="text/javascript">' + scriptResult + '</script></body></html>';
-  codeResult.src = 'data:text/html;charset=utf-8,' + escape(contentDOM);
-  // Another way to write iFrame
-  // let docu = iframeOM.contentWindow.document;
-  // docu.open(); docu.writeln(contentDOM); docu.close();
+    let contentDOM = '<html><head><style>*,*::before,*::after{box-sizing:border-box}html{overflow:auto}body{margin:0;padding:0;height:100%;overflow:auto}</style><style>' + cssResult + '</style></head><body>' + htmlResult + '<script type="text/javascript">' + scriptResult + '</script></body></html>';
+    codeResult.src = 'data:text/html;charset=utf-8,' + escape(contentDOM);
 }
 
-const handleRunClick = () => {
-  htmlResult = replaceChar(editors[0].innerHTML);
-  scriptResult = replaceChar(editors[1].innerHTML);
-  insertResults();
-};
-
-const handleCheckbox = e => {
-  insantCompile = e.target.checked;
-}
-
-const highlite = el => {
-  const dataLang = el.dataset.lang;                                               // Detect "js", "html", "py", "bash", ...
-  const langObj = lang[dataLang];                                                 // Extract object from lang regexes dictionary
-  let html = el.innerHTML;
-  Object.keys(langObj).forEach(key => {
-    html = html.replace(langObj[key], `<i class=${dataLang}_${key}>$1</i>`);
-  });
-  el.previousElementSibling.innerHTML = html;                                     // Finally, show highlights!
-  if (insantCompile) {
-    if (dataLang === 'js') scriptResult = replaceChar(el.innerHTML);
-    else if (dataLang === 'html') htmlResult = replaceChar(el.innerHTML);
+const handleRun = () => {
+    htmlResult = editors[0].value;
+    cssResult = editors[1].value;
+    scriptResult = editors[2].value;
     insertResults();
-  }
 };
 
-editors.forEach(el => {
-  el.contentEditable = true;
-  el.spellcheck = false;
-  el.autocorrect = 'off';
-  el.autocapitalize = 'off';
-  el.addEventListener('input', highlite.bind(null, el));
-  highlite(el);                                                                   // Init!
-});
+editors.forEach(t => t.addEventListener('input', () => { if (insantCompile) handleRun(); }));
+runButton.addEventListener('click', handleRun);
+instantRun.addEventListener('change', e => insantCompile = e.target.checked);
 
-runButton.addEventListener('click', handleRunClick);
-instantRun.addEventListener('change', handleCheckbox);
-// https://stackoverflow.com/questions/41884969/highlight-syntax-in-contenteditable
+(async $ => {
+    $('textarea.language-html.fill').forEach(t => t.value = `
+<div style="padding: 1px 20px;">
+	<div id="id01">
+		<h2>Code Playground</h2>
+	</div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+	<div><h2 class="head">Code Playground</h2></div>
+</div>
+`);
+
+    $('textarea.language-css.fill').forEach(t => {
+        t.value = `
+.head {
+	color: gray;
+}
+`;
+        t.dispatchEvent(new InputEvent('input'));
+    });
+
+    $('textarea.language-js.fill').forEach(t => {
+        t.value = `
+// Type some JavaScript here
+console.log('OK');
+document.getElementById('id01').style = "color: red;";
+`;
+        t.dispatchEvent(new InputEvent('input'));
+    });
+})(Bliss.$);
---
+++ projects/language-app/create/create.js
@@ -0,0 +1,88 @@
+const result = document.getElementById('result');
+const restxt = document.getElementById('restxt');
+const definition = document.getElementById('definition');
+const en = document.getElementById('en');
+const tr = document.getElementById('tr');
+
+let en_value = '', tr_value = '';
+let img = '', quo = '', def = '';
+let type = 'sentence';
+
+function handleChange() {
+    let image = img.length && img.trim().length ? `,\n    "img": "${img}"` : '';
+    let quote = quo.length && quo.trim().length ? `,\n    "quotes": "${quo}"` : '';
+    let defin = def.length && def.trim().length ? `,\n    "def": "${def}"` : '';
+    result.innerText = `, {
+    "type": "${type}",
+    "en": "${en_value}",
+    "tr": "${tr_value}",
+    "en_s": "",
+    "tr_s": ""${image}${quote}${defin}
+}`;
+}
+
+function handleCheck(radio) {
+    type = radio.value;
+    if (type === 'word') definition.removeAttribute('style');
+    else definition.style.display = 'none';
+    handleChange();
+}
+
+document.getElementById('enb').addEventListener('click', () => {
+    fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=tr&dt=bd&dt=t&dj=1&q=' + en_value)
+        .then(res => res.json())
+        .then(data => {
+            let translation = '';
+            for (const sentence of data.sentences) translation += sentence.trans;
+            tr.value = translation;
+            tr_value = translation;
+            handleChange();
+        });
+});
+
+document.getElementById('trb').addEventListener('click', () => {
+    fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=bd&dt=t&dj=1&q=' + tr_value)
+        .then(res => res.json())
+        .then(data => {
+            let translation = '';
+            for (const sentence of data.sentences) translation += sentence.trans;
+            en.value = translation;
+            en_value = translation;
+            handleChange();
+        });
+});
+
+en.addEventListener('input', e => {
+    en_value = e.target.value;
+    handleChange();
+});
+
+tr.addEventListener('input', e => {
+    tr_value = e.target.value;
+    handleChange();
+});
+
+document.getElementById('img').addEventListener('input', e => {
+    img = e.target.value;
+    handleChange();
+});
+
+document.getElementById('quo').addEventListener('input', e => {
+    quo = e.target.value;
+    handleChange();
+});
+
+document.getElementById('def').addEventListener('input', e => {
+    def = e.target.value;
+    handleChange();
+});
+
+document.getElementById('copy').addEventListener('click', e => {
+    const copied = e.target.nextElementSibling;
+    const text = result.innerText;
+    restxt.textContent = text;
+    restxt.select();
+    document.execCommand('copy');
+    copied.style.opacity = 1;
+    setTimeout(() => copied.removeAttribute('style'), 1000);
+});
