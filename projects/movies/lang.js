37130d7c409091c07bbf831a6a038d9a2a2221e1
ibrahimakin <ibrahim-akin@msn.com>
2023-05-11 07:58:47 +0300

    refactoring

--- projects/movies/lang.js
+++ projects/movies/lang.js
@@ -4,15 +4,19 @@ const lang_obj = {
         search: 'Ara',
         year: 'Yıl',
         details: 'Detaylar',
+        min: 'dak',
         director: 'Yönetmen',
-        cast: 'Oyuncular'
+        cast: 'Oyuncular',
+        placeholder: 'Film, dizi, bölüm adı giriniz'
     },
     en: {
         movies: 'MOVIES',
         search: 'Search',
         year: 'Year',
         details: 'Details',
+        min: 'min',
         director: 'Director',
-        cast: 'Cast'
+        cast: 'Cast',
+        placeholder: 'Enter movie, series, episode name'
     }
 };
