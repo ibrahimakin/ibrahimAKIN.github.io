68cedce85f9fe36eacb9cf3da8ad63b1cf8e9b3e
Vestel <ibrahim-akin@msn.com>
2023-08-28 08:40:38 +0300

    refactoring

--- components/navigation/sidenav/sidenav.js
+++ components/navigation/sidenav/sidenav.js
@@ -5,28 +5,29 @@ class Sidenav extends HTMLElement {
             <div id="sidenav">
                 <div class="main">
                     <div>
-                        <a title="Home" class="filled home icon" href="/" lang-tag="nav_home"></a>
+                        <a title="Home" class="filled home" href="/" lang-tag="nav_home"></a>
                         <span lang-tag="nav_home">Home</span>
                     </div>
                     <div></div>
                     <div class="menu">
                         <input title="Projects" class="pin-menu" type="checkbox" lang-tag="projects" tabindex="-1">
-                        <div class="filled project icon"></div>
+                        <div class="filled project"></div>
+                        <span lang-tag="projects">Projects</span>
                         <div class="expand">
                             <div>
-                                <a title="Language App" class="filled smile-n icon" href="/projects/language-app"></a>
+                                <a title="Language App" class="filled smile-n" href="/projects/language-app"></a>
                                 <span>Language App</span>
                                 <div>
-                                    <a title="Code Playground" class="filled code icon" href="/projects/code-playground"></a>
+                                    <a title="Code Playground" class="filled code" href="/projects/code-playground"></a>
                                     <span>Code Playground</span>
                                     <div>
-                                        <a title="Face Detector" class="filled face icon" href="/projects/face-detector" lang-tag="face_detector"></a>
+                                        <a title="Face Detector" class="filled face" href="/projects/face-detector" lang-tag="face_detector"></a>
                                         <span lang-tag="face_detector">Face Detector</span>
                                         <div>
-                                            <a title="Movies" class="filled movies icon" href="/projects/movies" lang-tag="nav_movies"></a>
+                                            <a title="Movies" class="filled movies" href="/projects/movies" lang-tag="nav_movies"></a>
                                             <span lang-tag="nav_movies">Movies</span>
                                             <div>
-                                                <a title="LCD Character Generator" class="filled iicon icon" href="/projects/lcd-character-generator" lang-tag="lcd"></a>
+                                                <a title="LCD Character Generator" class="filled iicon" href="/projects/lcd-character-generator" lang-tag="lcd"></a>
                                                 <span lang-tag="lcd">LCD Character Generator</span>
                                             </div>
                                         </div>
@@ -34,57 +35,56 @@ class Sidenav extends HTMLElement {
                                 </div>
                             </div>
                         </div>
-                        <span lang-tag="projects">Projects</span>
                     </div>
                     <div class="menu">
                         <input title="Games" class="pin-menu" type="checkbox" lang-tag="games" tabindex="-1">
-                        <div class="filled games icon"></div>
-                        <div class="expand more" style="--extra:1">
+                        <div class="filled games"></div>
+                        <span lang-tag="games">Games</span>
+                        <div class="expand more">
                             <div class="up">
-                                <a title="Tetris" class="filled tetris icon" href="/games/tetris"></a>
+                                <a title="Tetris" class="filled tetris" href="/games/tetris"></a>
                                 <span>Tetris</span>
                             </div>
                             <div>
-                                <a title="Snake" class="filled snake icon" href="/games/snake"></a>
+                                <a title="Snake" class="filled snake" href="/games/snake"></a>
                                 <span>Snake</span>
                                 <div>
-                                    <a title="Bird" class="filled bird icon" href="/games/bird"></a>
+                                    <a title="Bird" class="filled bird" href="/games/bird"></a>
                                     <span>Bird</span>
                                     <div>
-                                        <a title="Tic Tac Toe" class="filled tic-tac-toe icon" href="/games/tic-tac-toe" lang-tag="xox"></a>
+                                        <a title="Tic Tac Toe" class="filled tic-tac-toe" href="/games/tic-tac-toe" lang-tag="xox"></a>
                                         <span lang-tag="xox">Tic Tac Toe</span>
                                         <div>
-                                            <a title="Ping Pong" class="filled pong icon" href="/games/pong" lang-tag="pong"></a>
+                                            <a title="Ping Pong" class="filled pong" href="/games/pong" lang-tag="pong"></a>
                                             <span lang-tag="pong">Ping Pong</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <span lang-tag="games">Games</span>
                     </div>
                     <div>
-                        <a title="Resume" class="filled resume icon" href="/resume" lang-tag="resume"></a>
+                        <a title="Resume" class="filled resume" href="/resume" lang-tag="resume"></a>
                         <span lang-tag="resume">Resume</span>
                     </div>
                     <div>
-                        <a title="Blog" class="filled blog icon" href="/blog"></a>
+                        <a title="Blog" class="filled blog" href="/blog"></a>
                         <span>Blog</span>
                     </div>
                     <div class="menu">
                         <input title="Settings" class="pin-menu" type="checkbox" lang-tag="settings" tabindex="-1">
-                        <div class="filled settings icon"></div>
-                        <div class="expand more" style="--extra:1">
+                        <div class="filled settings"></div>
+                        <span lang-tag="settings">Settings</span>
+                        <div class="expand more">
                             <div class="up">
-                                <div title="Türkçe" lang-name="tr" class="filled tr icon lang-button" tabindex="0" ${lang_support}></div>
+                                <div title="Türkçe" lang-name="tr" class="filled tr lang-button" tabindex="0" ${lang_support}></div>
                                 <span>${lang_support ? 'Türkçe' : lang_obj.tr.nav_turkish}</span>
                             </div>
                             <div>
-                                <div title="English" lang-name="en" class="filled en icon lang-button" tabindex="0" ${lang_support}></div>
+                                <div title="English" lang-name="en" class="filled en lang-button" tabindex="0" ${lang_support}></div>
                                 <span>English</span>
                             </div>
                         </div>
-                        <span lang-tag="settings">Settings</span>
                     </div>
                 </div>
             </div>
