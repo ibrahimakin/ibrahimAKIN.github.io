b4e767af1e0bdf88be2f7a7c875f44dc44b2086d
Vestel <ibrahim-akin@msn.com>
2024-01-09 08:04:40 +0300

    add 404 not found

---
+++ components/not-found/lang.js
@@ -0,0 +1,10 @@
+const lang_obj = {
+    tr: {
+        page: 'Sayfa bulunamadı!',
+        lost: 'Görünüşe göre kaybolmuşsun.'
+    },
+    en: {
+        page: 'Page not found!',
+        lost: 'Looks like you got lost.'
+    }
+};
---
+++ components/not-found/not-found.js
@@ -0,0 +1,4 @@
+// https://codepen.io/rafaelavlucas/pen/NWWQNjZ
+// Parallax Code
+const scene = document.getElementById('scene');
+const parallax = new Parallax(scene);
