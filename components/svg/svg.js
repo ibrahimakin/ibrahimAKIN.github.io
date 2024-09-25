699105def23e2959f79e57f7e52c05527495db5e
Vestel <ibrahim-akin@msn.com>
2024-09-25 07:56:08 +0300

    update navigation icon path

--- components/svg/svg.js
+++ components/svg/svg.js
@@ -1,32 +1,30 @@
 function readSVGFile(file, className) {
-    let rawFile = new XMLHttpRequest();
+    const rawFile = new XMLHttpRequest();
     rawFile.open('GET', file);
     rawFile.setRequestHeader('Content-Type', 'image/svg+xml');
     rawFile.addEventListener('load', () => {
-        if (rawFile.readyState === 4) {
-            if (rawFile.status === 200 || rawFile.status == 0) {
-                let allText = rawFile.responseText;
-                for (const el of document.getElementsByClassName(className)) {
-                    el.innerHTML = allText;
-                }
+        if (rawFile.readyState === 4 && (rawFile.status === 200 || rawFile.status == 0)) {
+            for (const el of document.getElementsByClassName(className)) {
+                el.innerHTML = rawFile.responseText;
             }
         }
     });
     rawFile.send(null);
 }
 function initSVG() {
-    readSVGFile('/assets/icons/navbar/home-icon-silhouette.svg', 'home-js');
-    readSVGFile('/assets/icons/navbar/project-solid.svg', 'project-js');
-    readSVGFile('/assets/icons/navbar/games.svg', 'games-js');
-    readSVGFile('/assets/icons/navbar/resume-and-cv.svg', 'resume-js');
-    readSVGFile('/assets/icons/navbar/movies.svg', 'movies-js');
-    readSVGFile('/assets/icons/navbar/i-icon.svg', 'iicon-js');
-    readSVGFile('/assets/icons/navbar/bird.svg', 'bird-js');
-    readSVGFile('/assets/icons/navbar/snake.svg', 'snake-js');
-    readSVGFile('/assets/icons/navbar/tetris.svg', 'tetris-js');
-    readSVGFile('/assets/icons/navbar/settings.svg', 'settings-js');
-    readSVGFile('/assets/icons/navbar/tic-tac-toe.svg', 'tic-tac-toe-js');
-    readSVGFile('/assets/icons/navbar/back.svg', 'back-js');
-    readSVGFile('/assets/icons/navbar/forward.svg', 'forward-js');
+    const path = '/assets/icons/navigation/';
+    readSVGFile(path + 'home-icon-silhouette.svg', 'home-js');
+    readSVGFile(path + 'project-solid.svg', 'project-js');
+    readSVGFile(path + 'games.svg', 'games-js');
+    readSVGFile(path + 'resume-and-cv.svg', 'resume-js');
+    readSVGFile(path + 'movies.svg', 'movies-js');
+    readSVGFile(path + 'i-icon.svg', 'iicon-js');
+    readSVGFile(path + 'bird.svg', 'bird-js');
+    readSVGFile(path + 'snake.svg', 'snake-js');
+    readSVGFile(path + 'tetris.svg', 'tetris-js');
+    readSVGFile(path + 'settings.svg', 'settings-js');
+    readSVGFile(path + 'tic-tac-toe.svg', 'tic-tac-toe-js');
+    readSVGFile(path + 'back.svg', 'back-js');
+    readSVGFile(path + 'forward.svg', 'forward-js');
 }
 initSVG();
