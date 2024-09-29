1533c04615cc4f833a6206481f36dd0c724a7c78
Vestel <ibrahim-akin@msn.com>
2024-09-29 09:44:06 +0300

    update lcd character generator

--- projects/lcd-character-generator/lcd-character-generator.js
+++ projects/lcd-character-generator/lcd-character-generator.js
@@ -5,14 +5,13 @@ let result = 0;
 function onClick(elmnt, dsply) {
     if (dsply) {
         let value = parseInt(elmnt.value);
-        if (elmnt.style.backgroundColor != 'black') {
-            elmnt.style.backgroundColor = 'black';
-            elmnt.style.color = 'white';
+        if (!elmnt.classList.contains('on')) {
+            elmnt.classList.add('on');
             document.getElementById(elmnt.name).innerHTML = 1;
             result += value;
         }
         else {
-            elmnt.removeAttribute('style');
+            elmnt.classList.remove('on');
             document.getElementById(elmnt.name).innerHTML = 0;
             result -= value;
         }
@@ -21,15 +20,15 @@ function onClick(elmnt, dsply) {
     else {
         let txt = elmnt.getAttribute('aria-label');
         let arr = txt.split('x');
-        if (elmnt.style.backgroundColor != 'black') {
+        if (!elmnt.classList.contains('on')) {
             sum[arr[1]] = sum[arr[1]] + +arr[0];
-            elmnt.style.backgroundColor = 'black';
+            elmnt.classList.add('on');
             document.getElementById(arr[1]).innerHTML = sum[+arr[1]];
             document.getElementById(arr[1] + arr[1]).innerHTML = sum[+arr[1]];
         }
         else {
             sum[arr[1]] = sum[arr[1]] - +arr[0];
-            elmnt.removeAttribute('style');
+            elmnt.removeAttribute('class');
             document.getElementById(arr[1]).innerHTML = sum[+arr[1]];
             document.getElementById(arr[1] + arr[1]).innerHTML = sum[+arr[1]];
         }
@@ -41,13 +40,13 @@ function onClear(dsply) {
         result = 0;
         document.getElementById('dr').innerHTML = 0;
         for (let i = 0; i < 8; i++) {
-            document.getElementById('i' + arr[i]).removeAttribute('style');
+            document.getElementById('i' + arr[i]).classList.remove('on');
             document.getElementById(arr[i]).innerHTML = 0;
         }
     }
     else {
         const buttons = document.querySelectorAll('#lcd button:not([lang-tag])');
-        for (const button of buttons) button.removeAttribute('style');
+        for (const button of buttons) button.removeAttribute('class');
         for (let i = 1; i < 9; i++) {
             document.getElementById(i + '' + i).innerHTML = 0;
             document.getElementById(i).innerHTML = 0;
