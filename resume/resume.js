0706f1a596295b878840c00a23fd5460a7e91690
Vestel <ibrahim-akin@msn.com>
2025-07-29 07:08:12 +0300

    update resume

--- resume/resume.js
+++ resume/resume.js
@@ -24,7 +24,7 @@ for (const el of document.querySelectorAll('a[href^="#"]')) {
         clearTimeout(timeout);
         if (target) target.removeAttribute('class');
         const id = e.currentTarget.getAttribute('href');
-        target = document.querySelector(id);
+        target = document.querySelector(id + '+h3');
         target.classList.add('focus');
         timeout = setTimeout(() => target.removeAttribute('class'), 2100);
     });
